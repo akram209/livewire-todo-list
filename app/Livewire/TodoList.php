d730f80df396e1644d96d70bcfac5e968d780630
Akram <125211704+akram209@users.noreply.github.com>
2024-07-01 18:35:20 +0300

    read todos from live search

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -5,9 +5,11 @@ namespace App\Livewire;
 use App\Models\Todo;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TodoList extends Component
 {
+    use WithPagination;
     #[Rule('required|min:3|max:50')]
     public $name;
 
@@ -23,7 +25,7 @@ class TodoList extends Component
     }
     public function render()
     {
-        $todos = Todo::latest()->get();
+        $todos = Todo::latest()->where('name', 'like', '%' . $this->search . '%')->paginate(5);
         return view('livewire.todo-list', [
             "todos" => $todos
         ]);
