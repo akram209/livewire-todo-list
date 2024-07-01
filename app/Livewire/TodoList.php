d9ad52064eb7fc60aca740f6706fd22a27179040
Akram <125211704+akram209@users.noreply.github.com>
2024-07-01 23:14:22 +0300

    impliment update and delete handling error

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -3,9 +3,11 @@
 namespace App\Livewire;
 
 use App\Models\Todo;
+use Exception;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class TodoList extends Component
 {
@@ -14,6 +16,9 @@ class TodoList extends Component
     public $name;
 
     public $search;
+    public $editTodoId;
+    #[Rule('required|min:3|max:50')]
+    public $newName;
 
     public function create()
     {
@@ -22,15 +27,45 @@ class TodoList extends Component
         $this->reset('name');
 
         session()->flash('message', " created successfully");
+        $this->resetPage();
     }
 
-    public function delete(Todo $todo)
+
+    public function complete(Todo $todo)
     {
-        $todo->delete();
+        $todo->update(['completed' => true]);
+    }
+    public function edit(Todo $todo)
+    {
+        $this->newName = $todo->name;
+        $this->editTodoId = $todo->id;
+    }
+    public function update()
+    {
+        $this->validateOnly('newName');
+        $todo = Todo::find($this->editTodoId);
+        $todo->update(['name' => $this->newName]);
+        $this->reset('newName', 'editTodoId');
+    }
+
+    public function cancel()
+    {
+        $this->reset('newName', 'editTodoId');
+    }
+    public function delete($todoId)
+    {
+        try {
+            $todo = Todo::findOrFail($todoId);
+            $todo->delete();
+        } catch (Exception $e) {
+            session()->flash('Error', 'Todo already deleted');
+        }
+        return;
     }
     public function render()
     {
-        $todos = Todo::latest()->where('name', 'like', '%' . $this->search . '%')->paginate(5);
+        $todos = Todo::latest()->where('name', 'like', '%' . $this->search . '%')->where('completed', false)->paginate(5);
+
         return view('livewire.todo-list', [
             "todos" => $todos
         ]);
